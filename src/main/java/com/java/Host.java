f0ef89a5f4d501b5bec2d628e7bec1e9c5e81f1c
doanHa <dmha@uh.edu>
2019-05-10 10:05:27 -0500

    changes to pom.xml

--- src/main/java/com/java/Host.java
+++ src/main/java/com/java/Host.java
@@ -2,8 +2,10 @@ package com.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Host {
 	public static void main(String[] args) {
 		SpringApplication.run(Host.class, args);
